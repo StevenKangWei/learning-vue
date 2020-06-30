3bedeb903e3f2c073011d6e53bb1b765fb23ace3
Steven Kang <kangweibaby@163.com>
2020-06-30 19:23:27 +0800

    update for login

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-      app 根组件
+    <router-view>
+        
+    </router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-}
+  name: "app"
+};
 </script>
 
 <style>
---
+++ shop/src/components/Login.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt />
+            </div>
+            <el-form :model="loginForm" class="login_form">
+                <el-form-item>
+                    <el-input
+                        v-model="loginForm.username"
+                        placeholder="请输入用户名"
+                        prefix-icon="el-icon-user"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input
+                        v-model="loginForm.password"
+                        placeholder="请输入密码"
+                        prefix-icon="el-icon-lock"
+                        type='password'
+                    ></el-input>
+                </el-form-item>
+                <el-form-item class="buttons">
+                    <el-button type="success">登录</el-button>
+                    <el-button type="primary">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loginForm: {
+                username: "zhangsan",
+                password: "123"
+            }
+        };
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background: #2b4b6b;
+    height: 100%;
+}
+
+.login_box {
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.avatar_box {
+    height: 100px;
+    width: 100px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background: #eee;
+    }
+}
+
+.buttons {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+</style>
