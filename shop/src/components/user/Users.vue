ee6be88895626a69cece8cd87d3150402caa23f0
Steven Kang <kangweibaby@163.com>
2020-07-10 05:39:13 +0800

    update user list

--- shop/src/components/user/Users.vue
+++ shop/src/components/user/Users.vue
@@ -1,11 +1,75 @@
 <template>
     <div>
-        <h3>用户列表</h3>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <el-card class="box-card">
+            <el-row :gutter="20">
+                <el-col :span="12">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加用户</el-button>
+                </el-col>
+            </el-row>
+            <el-table :data="users" stripe>
+                <el-table-column type="index" label="#"></el-table-column>
+                <el-table-column prop="username" label="姓名"></el-table-column>
+                <el-table-column prop="email" label="邮箱"></el-table-column>
+                <el-table-column prop="mobile" label="电话"></el-table-column>
+                <el-table-column prop="role_name" label="角色"></el-table-column>
+                <el-table-column label="状态">
+                    <template slot-scope="scope">
+                        <el-switch v-model="scope.row.mg_state"></el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作" width="200px">
+                    <template slot-scope="scope">
+                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                        <el-tooltip effect="dark" content="修改角色" placement="top" :enterable="false">
+                            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                        </el-tooltip>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            queryinfo: {
+                query: '',
+                pagenum: 1,
+                pagesize: 2
+            },
+            users: [],
+            total: 0
+        }
+    },
+    created() {
+        this.get_users()
+    },
+    methods: {
+        async get_users() {
+            const { data: res } = await this.$http.get('users', {
+                params: this.queryinfo
+            })
+            if (res.meta.status != 200) return this.$message.error(res.meta.msg)
+            this.users = res.data.users
+            this.total = res.data.total
+            console.log(res)
+        }
+    }
+}
 </script>
 
 <style lang="less" scoped>
